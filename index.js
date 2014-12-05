31d6017b54640bb2481eb7f108de62f1cf4b45d0
Ben Martin <ben@benmartinstudios.com.au>
2014-12-05 16:52:54 +1000

    Initialized repo with module stub and added functionality to listen for show/hide events.

---
+++ index.js
@@ -0,0 +1,25 @@
+var EventEmitter = require("events").EventEmitter;
+var ref = new EventEmitter();
+
+var spawn = require('child_process').spawn;
+reflektor = spawn('reflektor');
+
+var _  = require('underscore');
+_.str = require('underscore.string');
+_.mixin(_.str.exports());
+_.str.include('Underscore.string', 'string');
+
+reflektor.stdout.on('data', function(data){
+	keyword = 'HIDE '
+
+	lines = data.toString().trim().split('\n');
+	lines.forEach(function(line){
+		if(_.startsWith(line, 'HIDE ')){
+			ref.emit('hide', line.split('HIDE ')[1]);
+		} else if(_.startsWith(line, 'SHOW ')){
+			ref.emit('show', line.split('SHOW ')[1]);
+		}
+	});
+});
+
+module.exports = ref;
